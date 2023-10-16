a1859d4b8697a1c6df1b9146296728342e9da598
constant <constant@immortal.game>
2023-10-16 13:12:00 +0200

    add file creation

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,3 +1,37 @@
+import components from "../components.json";
+import { exec } from "child_process";
+import fs from "fs";
+
 export const initCommandAction = async () => {
   console.log("Init command executed");
+
+  for (let i = 0; i < components.length; i++) {
+    const component = components[i];
+    const { name, content, packagesRequired } = component;
+
+    //Install packages
+    if (packagesRequired) {
+      exec(
+        `npm install ${packagesRequired}`,
+        (err: any, stdout: any, stderr: any) => {
+          if (err) {
+            console.log(err);
+          }
+          console.log(stdout);
+        }
+      );
+    }
+
+    //Create files with fs module
+    if (content) {
+      fs.writeFile(name, content, (err: any) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(`[${name}] component created`);
+      });
+    }
+  }
+
+  console.log("Design system imported successfully !");
 };
